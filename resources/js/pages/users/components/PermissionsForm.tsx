be6cbc6056a9a479b350ea70e4278984812cd41d
JesuMa28 <jesbram36@gmail.com>
2025-03-24 11:27:12 +0100

    CheckBoxes map

--- resources/js/pages/users/components/PermissionsForm.tsx
+++ resources/js/pages/users/components/PermissionsForm.tsx
@@ -10,6 +10,8 @@ import { toast } from "sonner";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/form"
 import { Separator } from "@/components/ui/separator";
+import { Users, PackageOpen, FileText, Settings } from 'lucide-react';
+import { useEffect } from "react";
 
 
 const permissionsGroup = {
@@ -42,12 +44,17 @@ const permissionsGroup = {
 
  } as const;
 
+
+const iconsMap: Record<keyof typeof permissionsGroup, React.ComponentType<{ className?: string }>> = {
+  users: Users,
+  products: PackageOpen,
+  reports: FileText,
+  settings: Settings,
+};
+
 export function PermissionsForm() {
     const form = useForm({
         defaultValues: {
-            verify_check: false,
-            verify_check1: false,
-            verify_check2: false,
             selector: "",
             items: ["view"],
         },
@@ -56,6 +63,32 @@ export function PermissionsForm() {
             toast.success("Formulario enviado con éxito");
         },
     });
+
+    // useEffect(() =>{
+    //     // Getting the role
+    //     const role = form.getValues("selector");
+
+    //     // Choosing the checked boxes by default with each role
+    //     if (role == "admin") {
+    //         Object.keys(permissionsGroup).forEach((category) => {
+    //             permissionsGroup[category as keyof typeof permissionsGroup].forEach((perm) => {
+    //                 form.setValues(`permissions.${category}.${perm.id}`, true);
+    //             });
+    //         });
+    //     } else if (role === "user") {
+    //         // Checking only the first and second perms of each section
+    //         Object.keys(permissionsGroup).forEach((category) => {
+    //             permissionsGroup[category as keyof typeof permissionsGroup].forEach((perm, index) => {
+    //                 if (index === 0 || index === 1) {
+    //                     form.setValue(`permissions.${category}.${perm.id}`, true);
+    //                 } else {
+    //                     form.setValue(`permissions.${category}.${perm.id}`, false);
+    //                 }
+    //             });
+    //         });
+    //     }
+    // }, [form.getValues("selector")]); // Este efecto se ejecuta cuando cambia el valor de "selector"
+
 
 
     return (
@@ -90,71 +123,45 @@ export function PermissionsForm() {
 
             <Separator/>
 
-            {/* Permissions Container */}
             <FormItem className="grid grid-cols-2 gap-4">
+                {Object.entries(permissionsGroup).map(([category, permissions]) => {
+                    const Icon = iconsMap[category as keyof typeof iconsMap];
 
-                {/* Users Perms */}
-                <Card>
-                    <CardContent>
-                        {/* Checkbox de verify_check */}
-                        <form.Field name="verify_check" defaultValue={false}>
-                            {(field) => (
-                                <div className="flex items-center space-x-2">
-                                    <Checkbox
-                                        checked={field.state.value}
-                                        onCheckedChange={(checked) => field.handleChange(checked === true)}
-                                    />
-                                    <Label>Suscribirse al boletín</Label>
+                    return (
+                        <Card key={category}>
+                            <CardHeader className="w-full">
+                                <div className="w-full flex items-center">
+                                    {Icon && <Icon className="w-4 h-4 mr-2 text-[#3b82f6]"/>}
+                                    <CardTitle className="w-5">
+                                        {category.charAt(0).toUpperCase() + category.slice(1)}
+                                    </CardTitle>
+
                                 </div>
+                            </CardHeader>
+                            <CardContent>
+                                {permissions.map((perm) => (
+                                    <form.Field key={perm.id} name={`permissions.${category}.${perm.id}`} defaultValue={false}>
+                                        {(field) => {
+                                            // Saving checboxes' Id for the htmlFor of the labels
+                                            const checkboxId = `checkbox-${category}-${perm.id}`;
+                                            return (
+                                                <div className="flex items-center space-x-2 mb-2">
+                                                    <Checkbox
+                                                        checked={Boolean(field.state.value)}
+                                                        onCheckedChange={(checked) => field.handleChange(checked === true)}
+                                                        className="border-[#3b82f6] data-[state=checked]:bg-[#3b82f6] data-[state=checked]:border-[#3b82f6]"
 
-
-                            )}
-                        </form.Field>
-                        <form.Field name="verify_check1" defaultValue={false}>
-                            {(field) => (
-                                <div className="flex items-center space-x-2">
-                                    <Checkbox
-                                        checked={field.state.value}
-                                        onCheckedChange={(checked) => field.handleChange(checked === true)}
-                                    />
-                                    <Label>Suscribirse al boletín</Label>
-                                </div>
-
-
-                            )}
-                        </form.Field>
-                        <form.Field name="verify_check2" defaultValue={false}>
-                            {(field) => (
-                                <div className="flex items-center space-x-2">
-                                    <Checkbox
-                                        checked={field.state.value}
-                                        onCheckedChange={(checked) => field.handleChange(checked === true)}
-                                    />
-                                    <Label>Suscribirse al boletín</Label>
-                                </div>
-
-
-                            )}
-                        </form.Field>
-                        <form.Field name="verify_check" defaultValue={false}>
-                            {(field) => (
-                                <div className="flex items-center space-x-2">
-                                    <Checkbox
-                                        checked={field.state.value}
-                                        onCheckedChange={(checked) => field.handleChange(checked === true)}
-                                    />
-                                    <Label>Suscribirse al boletín</Label>
-                                </div>
-
-
-                            )}
-                        </form.Field>
-
-
-                    </CardContent>
-                </Card>
-
-
+                                                    />
+                                                    <Label htmlFor={checkboxId}>{perm.label}</Label>
+                                                </div>
+                                            );
+                                        }}
+                                    </form.Field>
+                                ))}
+                            </CardContent>
+                        </Card>
+                    );
+                })}
             </FormItem>
 
             {/* Botón de envío */}
